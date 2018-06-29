eb6cf16287a14133399bdcdcac0f63959b74125d
stevenhao <stevenhao@gmail.com>
2018-06-29 21:19:08 +0800

    allow cors

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import _ from 'lodash';
 import url from 'url';
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/a.png', (req, res) => {
   res.sendFile('images/a.png', {
     root: __dirname,
